44ff5fc5f359e950d348d5c024a3c224f241dc4e
Philip Campbell <flipcampbell@gmail.com>
2015-08-08 19:10:02 -0500

    created trust-tree dir and index

---
+++ case-studies/trust-tree/index.php
@@ -0,0 +1,13 @@
+<?php
+  // include('../inc/contact-mailer.php');
+  include('../../inc/header.php');
+?>
+
+<div class="">
+  <?php include('../../inc/nav.php');?>
+</div>
+
+<?php
+  // include('../inc/contact-form.php');
+  include('../../inc/footer.php');
+?>
