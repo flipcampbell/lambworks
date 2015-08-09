8863e4b1a3fb3d0f2c23d9ac3db5dcdb4db28190
Philip Campbell <flipcampbell@gmail.com>
2015-08-08 20:52:41 -0500

    updated trusttree index

--- case-studies/trust-tree/index.php
+++ case-studies/trust-tree/index.php
@@ -3,9 +3,23 @@
   include('../../inc/header.php');
 ?>
 
-<div class="">
+<div class="hero case-study bg-trusttree">
   <?php include('../../inc/nav.php');?>
+  <div class="large-12 large-centered columns">
+    <div class="fade-in-second">
+      <h1 class="text-center fade-in-second font-white">
+        Trust Tree
+      </h1>
+    </div>
+  </div>
+<div class="large-6 small-12 small-centered large-centered columns">
+  <div class="fade-in-second">
+    <h2 class="text-center fade-in-second font-white">
+      Register your trademark through IP attorneys, without stepping foot in an attorney’s office.
+    </h2>
+  </div>
 </div>
+</div> <!-- /.hero-->
 
 <?php
   // include('../inc/contact-form.php');
