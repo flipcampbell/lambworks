4d65c539620407e303a6dc4642f3f3f45886d53f
Philip Campbell <flipcampbell@gmail.com>
2015-08-10 21:35:13 -0500

    updated homepage screenshot and added more content

--- case-studies/trust-tree/index.php
+++ case-studies/trust-tree/index.php
@@ -4,7 +4,6 @@
 ?>
 <?php include('../../inc/nav.php');?>
 <div class="hero-case-study bg-trusttree no-margin-bottom">
-
   <div class="large-12 large-centered columns">
     <div class="fade-in-second">
       <h1 class="text-center fade-in-second font-white">
@@ -22,9 +21,9 @@
 </div> <!-- /.hero-->
 <div class="bg-gradient">
   <section class="bg-white">
-    <div class="row font-slate">
+    <div class="row font-slate padding-bottom-2rem">
       <div class="large-4 large-uncentered medium-uncentered small-12 small-centered columns text-center">
-        <img src="/../images/case-studies/trust-tree/trust-tree-logo.png" alt="Trust Tree Logo">
+        <img src="/images/case-studies/trust-tree/trust-tree-logo.png" alt="Trust Tree Logo">
       </div>
       <div class="large-4 large-uncentered medium-6 medium-uncentered small-12 small-centered columns">
         <h5 class="no-margin-bottom font-slate"><span>Client</span></h5>
@@ -44,11 +43,11 @@
       </div> <!-- /.large-4 large-uncentered medium-6 medium-uncentered small-12 small-centered columns-->
     </div> <!-- /.row-->
     <div class="row">
-      <div class="large-10 small-12 small-centered columns text-center">
-        <img src="/../images/case-studies/trust-tree/trust-tree-homepage.jpg" alt="Trust Tree Homepage">
+      <div class="small-12 small-centered columns text-center padding-bottom-2rem">
+        <img src="/images/case-studies/trust-tree/trust-tree-homepage.png" alt="Trust Tree Homepage">
       </div> <!-- /.small-10 small-centered columns text-center-->
     </div> <!-- /.row-->
-    <h3 class="text-center font-grey">The Story</h3>
+    <h3 class="text-center font-grey padding-bottom-1rem">The Story</h3>
     <p class="large-6 medium-8 small-10 small-centered columns font-grey">
       Bill, Kevin, and Randy needed a brand and online platform to transition
       their in-person trademark registration services into a streamlined digital offering.
@@ -57,16 +56,16 @@
       Anyone can file a trademark application online through the USPTO, but the
       application and process are quite confusing if you’re not an attorney.
     </p>
-    <p class="large-6 medium-8 small-10 small-centered columns font-grey">
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey padding-bottom-2rem">
       Trust Tree is a simplified and attorney-guided service for anyone who
       wants to file a trademark online, but doesn’t want to step foot in an attorney’s office.
     </p>
     <div class="row">
-      <div class="small-12 small-centered columns text-center">
-        <img src="/../images/case-studies/trust-tree/trust-tree-pages.png" alt="Trust Tree Pages">
+      <div class="small-12 small-centered columns text-center padding-bottom-2rem">
+        <img src="/images/case-studies/trust-tree/trust-tree-pages.png" alt="Trust Tree Pages">
       </div> <!-- /.small-10 small-centered columns text-center-->
     </div> <!-- /.row-->
-    <h3 class="medium-7 small-12 small-centered columns text-center font-grey">
+    <h3 class="medium-7 small-12 small-centered columns text-center font-grey padding-bottom-1rem">
       Defining the audience, understanding their problem, and offering a service
       to solve that problem
     </h3>
@@ -77,7 +76,7 @@
       thought patterns of those who tend to use the online trademark registration
       services offered by competitors.
     </p>
-    <p class="large-6 medium-8 small-10 small-centered columns font-grey">
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey padding-bottom-2rem">
       The average business owner's experience with the trademarking process varies
       so much that it made sense to break the service up into levels
       (we called them packages) based on how much hand-holding a customer prefers.
@@ -86,8 +85,65 @@
       Multi-tiered pricing also lets Trust Tree price anchor against themselves,
       rather than the competition.
     </p>
+    <div class="row">
+      <div class="small-12 small-centered columns text-center padding-bottom-2rem">
+        <img src="/images/case-studies/trust-tree/trust-tree-planning.png" alt="Trust Tree Storyboards">
+      </div> <!-- /.small-10 small-centered columns text-center-->
+    </div> <!-- /.row-->
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey padding-bottom-2rem">
+      We storyboarded the entire user experience from the first point of
+      onboarding, to actually signing up, completing a trademark form, events
+      that occur during that application process, and maintaining a trademark over time.
+    </p>
+    <div class="row">
+      <div class="small-12 small-centered columns text-center padding-bottom-2rem">
+        <img src="/images/case-studies/trust-tree/trust-tree-web-app.png" alt="Trust Tree Web App">
+      </div> <!-- /.small-10 small-centered columns text-center-->
+    </div> <!-- /.row-->
+    <h3 class="text-center font-grey padding-bottom-1rem">
+      Visual Identity, Branding &amp; Tone
+    </h3>
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey">
+      The logo needed to be strong, memorable, and simple to begin building a brand
+      for this new startup. The design behind the Trust Tree logo signifies trust
+      and growth, as derived from the name, through the symbolic combination of a tree and key.
+    </p>
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey padding-bottom-2rem">
+      We established that the tone used through all communications need to
+      represent the brand in a friendly and educational way, by using helpful, plain language.
+    </p>
+    <div class="row">
+      <div class="small-12 small-centered columns text-center padding-bottom-2rem">
+        <img src="/images/case-studies/trust-tree/trust-tree-identity-guide.png" alt="Trust Tree Identiy Guide">
+      </div> <!-- /.small-10 small-centered columns text-center-->
+    </div> <!-- /.row-->
+    <div class="row">
+      <div class="small-12 small-centered columns text-center padding-bottom-2rem">
+        <img src="/images/case-studies/trust-tree/trust-tree-business-cards.png" alt="Trust Tree Business Cards">
+      </div> <!-- /.small-10 small-centered columns text-center-->
+    </div> <!-- /.row-->
+    <div class="row">
+      <div class="small-12 small-centered columns text-center padding-bottom-2rem">
+        <img src="/images/case-studies/trust-tree/trust-tree-magazine-ad.png" alt="Trust Tree Magazine Ad">
+      </div> <!-- /.small-10 small-centered columns text-center-->
+    </div> <!-- /.row-->
   </section> <!-- /.section-->
 </div> <!-- /.bg-gradient-->
+
+<section class="bg-plaster">
+  <div class="row">
+    <div class="medium-7 small-12 small-centered columns">
+      <div class="large-6 large-uncentered small-12 small-centered columns">
+        <h4 class="font-slate text-center">Working on something great?</h4>
+      </div> <!-- /.medium-6 small-12 small-centered columns-->
+      <div class="large-6 large-uncentered small-12 small-centered columns text-center">
+        <a href="/#contact" class="button radius text-center bg-magenta text-center">
+          Get in touch
+        </a>
+      </div>
+    </div> <!-- /.small-8 small-centered columns-->
+  </div> <!-- /.row-->
+</section> <!-- /.section-->
 
 <?php
   // include('../inc/contact-form.php');
