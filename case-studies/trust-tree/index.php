ab62c2b8f7743837efa00eed41e37e8586e85dcb
Philip Campbell <flipcampbell@gmail.com>
2015-08-10 19:28:06 -0500

    updated case study content

--- case-studies/trust-tree/index.php
+++ case-studies/trust-tree/index.php
@@ -20,35 +20,74 @@
     </div> <!-- /.fade-in-second-->
   </div> <!-- /.large-12 large-centered columns-->
 </div> <!-- /.hero-->
-
-<section class="bg-white">
-  <div class="row font-slate">
-    <div class="large-4 large-uncentered medium-uncentered small-12 small-centered columns text-center">
-      <img src="/../images/case-studies/trust-tree/trust-tree-logo.png" alt="Trust Tree Logo">
-    </div>
-    <div class="large-4 large-uncentered medium-6 medium-uncentered small-12 small-centered columns">
-      <h5 class="no-margin-bottom font-slate"><span>Client</span></h5>
-      <p>Trust Tree Legal, PC</p>
-      <h5 class="no-margin-bottom font-slate"><span>Timeline</span></h5>
-      <p>December 2014 - Present</p>
-    </div>
-    <div class="large-4 large-uncentered medium-6 medium-uncentered small-12 small-centered columns">
-      <h5 class="no-margin-bottom font-slate"><span>Services Provided</span></h5>
-      <p>Product Strategy, Visual Identity and Branding, Web App UX/UI Design</p>
-      <h5 class="no-margin-bottom font-slate"><span>Project URL</span></h5>
-      <p>
-        <a href="trusttreetrademarks.com" target="_blank" class="font-slate underlined">
-          trusttreetrademarks.com
-        </a>
-      </p>
-    </div>
-  </div> <!-- /.row-->
-  <div class="row">
-    <div class="large-10 small-12 small-centered columns text-center">
-      <img src="/../images/case-studies/trust-tree/trust-tree-homepage.jpg" alt="Trust Tree Homepage">
-    </div> <!-- /.small-10 small-centered columns text-center-->
-  </div> <!-- /.row-->
-</section> <!-- /.section-->
+<div class="bg-gradient">
+  <section class="bg-white">
+    <div class="row font-slate">
+      <div class="large-4 large-uncentered medium-uncentered small-12 small-centered columns text-center">
+        <img src="/../images/case-studies/trust-tree/trust-tree-logo.png" alt="Trust Tree Logo">
+      </div>
+      <div class="large-4 large-uncentered medium-6 medium-uncentered small-12 small-centered columns">
+        <h5 class="no-margin-bottom font-slate"><span>Client</span></h5>
+        <p>Trust Tree Legal, PC</p>
+        <h5 class="no-margin-bottom font-slate"><span>Timeline</span></h5>
+        <p>December 2014 - Present</p>
+      </div>
+      <div class="large-4 large-uncentered medium-6 medium-uncentered small-12 small-centered columns">
+        <h5 class="no-margin-bottom font-slate"><span>Services Provided</span></h5>
+        <p>Product Strategy, Visual Identity and Branding, Web App UX/UI Design</p>
+        <h5 class="no-margin-bottom font-slate"><span>Project URL</span></h5>
+        <p>
+          <a href="trusttreetrademarks.com" target="_blank" class="font-slate underlined">
+            trusttreetrademarks.com
+          </a>
+        </p>
+      </div>
+    </div> <!-- /.row-->
+    <div class="row">
+      <div class="large-10 small-12 small-centered columns text-center">
+        <img src="/../images/case-studies/trust-tree/trust-tree-homepage.jpg" alt="Trust Tree Homepage">
+      </div> <!-- /.small-10 small-centered columns text-center-->
+    </div> <!-- /.row-->
+    <h3 class="text-center font-grey">The Story</h3>
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey">
+      Bill, Kevin, and Randy needed a brand and online platform to transition
+      their in-person trademark registration services into a streamlined digital offering.
+    </p>
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey">
+      Anyone can file a trademark application online through the USPTO, but the
+      application and process are quite confusing if you’re not an attorney.
+    </p>
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey">
+      Trust Tree is a simplified and attorney-guided service for anyone who
+      wants to file a trademark online, but doesn’t want to step foot in an attorney’s office.
+    </p>
+    <div class="row">
+      <div class="small-12 small-centered columns text-center">
+        <img src="/../images/case-studies/trust-tree/trust-tree-pages.png" alt="Trust Tree Pages">
+      </div> <!-- /.small-10 small-centered columns text-center-->
+    </div> <!-- /.row-->
+    <h3 class="medium-7 small-12 small-centered columns text-center font-grey">
+      Defining the audience, understanding their problem, and offering a service
+      to solve that problem
+    </h3>
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey">
+      Our first priority was to make sure we were all in agreement about who the
+      target customer was. Together, we created assumption personas based on
+      common elements that in-person customers shared, as well as behaviors and
+      thought patterns of those who tend to use the online trademark registration
+      services offered by competitors.
+    </p>
+    <p class="large-6 medium-8 small-10 small-centered columns font-grey">
+      The average business owner's experience with the trademarking process varies
+      so much that it made sense to break the service up into levels
+      (we called them packages) based on how much hand-holding a customer prefers.
+      We modeled this structure after TurboTax, as users can self-select
+      the desired level of help they receive when filing taxes online.
+      Multi-tiered pricing also lets Trust Tree price anchor against themselves,
+      rather than the competition.
+    </p>
+  </section> <!-- /.section-->
+</div>
 
 <?php
   // include('../inc/contact-form.php');
