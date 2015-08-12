0ce7ba4e9d0af82631bc37b43f0c0543ebe08903
Philip Campbell <flipcampbell@gmail.com>
2015-08-12 18:43:22 -0500

    added content to zeumo case study

--- case-studies/zeumo/index.php
+++ case-studies/zeumo/index.php
@@ -3,37 +3,40 @@
   include('../../inc/header.php');
 ?>
 
-<div class="hero-case-study bg-trusttree no-margin-bottom">
+<div class="hero-case-study bg-zeumo no-margin-bottom">
   <?php include('../../inc/nav.php');?>
   <div class="large-12 large-centered columns">
     <h1 class="text-center fade-in-second font-white">
-      Trust Tree
+      Zeumo
     </h1>
   </div> <!-- /.large-12 large-centered columns-->
   <div class="large-5 medium-8 small-12 small-centered columns">
     <h2 class="text-center fade-in-second font-white no-margin-bottom">
-      Register your trademark through IP attorneys, without stepping foot in an attorney’s office.
+      A customizable, mobile and social platform that lets healthcare
+      organizations have their own social network.
     </h2>
   </div> <!-- /.large-12 large-centered columns-->
 </div> <!-- /.hero-->
 <section class="bg-plaster">
   <div class="row font-slate">
-    <div class="large-4 large-uncentered medium-uncentered small-12 small-centered columns text-center">
-      <img src="/images/case-studies/trust-tree/trust-tree-logo.png" alt="Trust Tree Logo">
+    <div class="large-4 large-uncentered medium-uncentered small-12 small-centered columns text-center padding-top-30px">
+      <img src="/images/case-studies/zeumo/zeumo-logo.png" alt="Zeumo Logo">
     </div>
-    <div class="large-4 large-uncentered medium-6 medium-uncentered small-12 small-centered columns">
+    <div class="large-3 large-uncentered medium-5 medium-uncentered small-12 small-centered columns">
       <h5 class="no-margin-bottom font-slate"><span>Client</span></h5>
-      <p>Trust Tree Legal, PC</p>
+      <p>Zeumo, Inc.</p>
       <h5 class="no-margin-bottom font-slate"><span>Timeline</span></h5>
-      <p>December 2014 - Present</p>
+      <p>January 2013 - August 2014</p>
     </div>
-    <div class="large-4 large-uncentered medium-6 medium-uncentered small-12 small-centered columns">
+    <div class="large-5 large-uncentered medium-7 medium-uncentered small-12 small-centered columns">
       <h5 class="no-margin-bottom font-slate"><span>Services Provided</span></h5>
-      <p>Product Strategy, Visual Identity and Branding, Web App UX/UI Design</p>
+      <p>Visual Identity Rebranding, Web &amp; Mobile App UI Design, Marketing
+        Pages Design, Email Campaign Design
+      </p>
       <h5 class="no-margin-bottom font-slate"><span>Project URL</span></h5>
       <p>
-        <a href="http://www.trusttreetrademarks.com" target="_blank" class="font-slate underlined">
-          trusttreetrademarks.com
+        <a href="http://www.zeumo.com" target="_blank" class="font-slate underlined">
+          zeumo.com
         </a>
       </p>
     </div> <!-- /.large-4 large-uncentered medium-6 medium-uncentered small-12 small-centered columns-->
@@ -43,89 +46,71 @@
   <div class="bg-gradient">
     <div class="row">
       <div class="small-12 small-centered columns text-center padding-bottom-2rem">
-        <img src="/images/case-studies/trust-tree/trust-tree-homepage.png" alt="Trust Tree Homepage">
+        <img src="/images/case-studies/zeumo/zeumo-homepage.png" alt="Zeumo Homepage">
       </div> <!-- /.small-10 small-centered columns text-center-->
     </div> <!-- /.row-->
     <h3 class="text-center font-grey padding-bottom-1rem">The Story</h3>
     <p class="large-4 medium-8 small-10 small-centered columns font-grey">
-      Bill, Kevin, and Randy needed a brand and online platform to transition
-      their in-person trademark registration services into a streamlined digital offering.
-    </p>
-    <p class="large-4 medium-8 small-10 small-centered columns font-grey">
-      Anyone can file a trademark application online through the USPTO, but the
-      application and process are quite confusing if you’re not an attorney.
+      Zeumo launched in 2012 as a customizable social platform for high schools.
+      It enabled students to learn about job and program opportunities, directly
+      connect to teachers, and gave students a voice about what's going on in their
+      school through daily polls.
     </p>
     <p class="large-4 medium-8 small-10 small-centered columns font-grey padding-bottom-2rem">
-      Trust Tree is a simplified and attorney-guided service for anyone who
-      wants to file a trademark online, but doesn’t want to step foot in an attorney’s office.
+      After discovering it was premature for the education system to regularly
+      implement this new technology, Zeumo quickly pivoted into the healthcare industry.
     </p>
     <div class="row">
       <div class="small-12 small-centered columns text-center padding-bottom-2rem">
-        <img src="/images/case-studies/trust-tree/trust-tree-pages.png" alt="Trust Tree Pages">
+        <img src="/images/case-studies/zeumo/zeumo-marketing-pages.png" alt="Zeumo Marketing Pages">
       </div> <!-- /.small-10 small-centered columns text-center-->
     </div> <!-- /.row-->
-    <h3 class="large-6 medium-8 small-12 small-centered columns text-center font-grey padding-bottom-1rem">
-      Defining the audience, understanding their problem, and offering a service
-      to solve that problem
-    </h3>
     <p class="large-4 medium-8 small-10 small-centered columns font-grey">
-      Our first priority was to make sure we were all in agreement about who the
-      target customer was. Together, we created assumption personas based on
-      common elements that in-person customers shared, as well as behaviors and
-      thought patterns of those who tend to use the online trademark registration
-      services offered by competitors.
-    </p>
-    <p class="large-4 medium-8 small-10 small-centered columns font-grey padding-bottom-2rem">
-      The average business owner's experience with the trademarking process varies
-      so much that it made sense to break the service up into levels
-      (we called them packages) based on how much hand-holding a customer prefers.
-      We modeled this structure after TurboTax, as users can self-select
-      the desired level of help they receive when filing taxes online.
-      Multi-tiered pricing also lets Trust Tree price anchor against themselves,
-      rather than the competition.
+      Zeumo now allows hospitals to communicate with their physicians through
+      phones and tablets and creates a network for staff collaboration. Physicians
+      on the go can rely less on email and quickly receive notifications and alerts.
     </p>
     <div class="row">
       <div class="small-12 small-centered columns text-center padding-bottom-2rem">
-        <img src="/images/case-studies/trust-tree/trust-tree-planning.png" alt="Trust Tree Storyboards">
-      </div> <!-- /.small-10 small-centered columns text-center-->
-    </div> <!-- /.row-->
-    <p class="large-4 medium-8 small-10 small-centered columns font-grey padding-bottom-2rem">
-      We storyboarded the entire user experience from the first point of
-      onboarding, to actually signing up, completing a trademark form, events
-      that occur during that application process, and maintaining a trademark over time.
-    </p>
-    <div class="row">
-      <div class="small-12 small-centered columns text-center padding-bottom-2rem">
-        <img src="/images/case-studies/trust-tree/trust-tree-web-app.png" alt="Trust Tree Web App">
+        <img src="/images/case-studies/zeumo/zeumo-web-app.png" alt="Zeumo Web App">
       </div> <!-- /.small-10 small-centered columns text-center-->
     </div> <!-- /.row-->
     <h3 class="text-center font-grey padding-bottom-1rem">
-      Visual Identity, Branding &amp; Tone
+      Visual Identity Rebranding
     </h3>
-    <p class="large-4 medium-8 small-10 small-centered columns font-grey">
-      The logo needed to be strong, memorable, and simple to begin building a brand
-      for this new startup. The design behind the Trust Tree logo signifies trust
-      and growth, as derived from the name, through the symbolic combination of a tree and key.
-    </p>
     <p class="large-4 medium-8 small-10 small-centered columns font-grey padding-bottom-2rem">
-      We established that the tone used through all communications need to
-      represent the brand in a friendly and educational way, by using helpful, plain language.
+      In 2014, Zeumo transitioned from being an education startup to a healthcare
+      startup and needed to rebrand. We wanted to keep the lightning bolt, but
+      make their identity more simplified and bold.
     </p>
+    <div class="row padding-bottom-2rem">
+      <div class="small-8 small-centered columns">
+        <div class="small-6 columns text-center padding-bottom-2rem">
+          <p class="uppercase font-grey">Before</p>
+          <img src="/images/case-studies/zeumo/zeumo-old-logo.png" alt="Zeumo Old Log">
+        </div> <!-- /.small-6 columns text-center padding-bottom-2rem-->
+        <div class="small-6 columns text-center padding-bottom-2rem">
+          <p class="uppercase font-grey">After</p>
+          <img src="/images/case-studies/zeumo/zeumo-new-logo.png" alt="Zeumo New Log" class="padding-top-56px">
+        </div> <!-- /.small-6 columns text-center padding-bottom-2rem-->
+      </div>
+    </div> <!-- /.row-->
     <div class="row">
       <div class="small-12 small-centered columns text-center padding-bottom-2rem">
-        <img src="/images/case-studies/trust-tree/trust-tree-identity-guide.png" alt="Trust Tree Identiy Guide">
+        <img src="/images/case-studies/zeumo/zeumo-logo-guide.png" alt="Zeumo Logo Guide">
       </div> <!-- /.small-10 small-centered columns text-center-->
     </div> <!-- /.row-->
     <div class="row">
       <div class="small-12 small-centered columns text-center padding-bottom-2rem">
-        <img src="/images/case-studies/trust-tree/trust-tree-business-cards.png" alt="Trust Tree Business Cards">
+        <img src="/images/case-studies/zeumo/zeumo-business-cards.png" alt="Zeumo Business Cards">
       </div> <!-- /.small-10 small-centered columns text-center-->
     </div> <!-- /.row-->
-    <div class="row">
-      <div class="small-12 small-centered columns text-center padding-bottom-2rem">
-        <img src="/images/case-studies/trust-tree/trust-tree-magazine-ad.png" alt="Trust Tree Magazine Ad">
-      </div> <!-- /.small-10 small-centered columns text-center-->
-    </div> <!-- /.row-->
+      <h3 class="text-center font-grey padding-bottom-1rem">
+        The Latest
+      </h3>
+      <p class="small-12 small-centered columns font-grey text-center">
+        Zeumo was acquired by The Advisor Board in the summer of 2015.
+      </p>
   </div> <!-- /.bg-gradient-->
 </section> <!-- /section.bg-white no-padding-top-->
 
@@ -145,17 +130,16 @@
   </div> <!-- /.row-->
   <div class="small-11 small-centered columns divider"></div>
   <div class="row">
-    <a href="#" class="medium-6 medium-uncentered small-12 small-centered columns">
+    <a href="/case-studies/trust-tree" class="medium-6 medium-uncentered small-12 small-centered columns">
       <div class="medium-3 medium-uncentered small-6 small-centered columns text-center clearfix">
-        <img src="/images/case-studies/zeumo-logo-icon.png" alt="Zeumo Logo" class="float-right padding-top-22px">
+        <img src="/images/case-studies/trust-tree-logo-icon.png" alt="Zeumo Logo" class="float-right padding-top-22px">
       </div> <!-- /.medium-3 medium-uncentered small-4 small-centered columns text-center-->
       <div class="medium-9 medium-uncentered small-10 small-centered columns">
-        <h4 class="font-magenta">Zeumo</h4>
+        <h4 class="font-magenta">Trust Tree</h4>
         <p class="font-slate">
-          King dungeon dirk honeyed locusts, lord of light,
-          always pays his debts m'lord moon-flower juice death before disgrace
-          betrothed. Dog spiced wine mare's milk, tread lightly here lance green
-          dreams ever vigilant we do not sow
+          Trust Tree is a fresh alternative to how we register trademarks.
+          The attorney-guided service is for anyone ready to file a trademark
+          online without stepping foot in an attorney’s office.
         </p>
       </div> <!-- /.medium-9 medium-uncentered small-8 small-centered columns-->
     </a> <!-- /.medium-6 medium-uncentered small-12 small-centered columns-->
@@ -166,10 +150,9 @@
       <div class="medium-9 medium-uncentered small-10 small-centered columns">
         <h4 class="font-magenta">Merch Colony</h4>
         <p class="font-slate">
-          King dungeon dirk honeyed locusts, lord of light,
-          always pays his debts m'lord moon-flower juice death before disgrace
-          betrothed. Dog spiced wine mare's milk, tread lightly here lance green
-          dreams ever vigilant we do not sow
+          Merch Colony is changing the way we recommend local businesses.
+          They’re like Yelp for your hometown, but with an emphasis on recommending
+          instead of reviewing.
         </p>
       </div> <!-- /.medium-9 medium-uncentered small-8 small-centered columns-->
     </a> <!-- /.medium-6 medium-uncentered small-12 small-centered columns-->
